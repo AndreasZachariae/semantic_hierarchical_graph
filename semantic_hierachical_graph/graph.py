5d332e9c76145cd25fafafaea0bbaa30f95485e6
Andreas Zachariae <zaan0001@hs-karlsruhe.de>
2022-10-20 01:49:38 +0200

    Tree data structure

--- semantic_hierachical_graph/graph.py
+++ semantic_hierachical_graph/graph.py
@@ -27,32 +27,61 @@ hierarchy_levels = {
 }
 
 
+class TreeNode:
+    def __init__(self, number, name):
+        self.number = number
+        self.name = name
+        self.children = []
+
+    def addChild(self, child):
+        self.children.append(child)
+
+    def serialize(self):
+        s = {}
+        for child in self.children:
+            s[child.name] = child.serialize()
+        return s
+
+
+class Level():
+    def __init__(self, parent: nx.Graph):
+        self.graph = nx.Graph()
+        self.parent: nx.Graph = parent
+
+
 class SHGraph():
     g: List[nx.Graph] = []
 
     def __init__(self, hierachy_level: int) -> None:
         self.g = [nx.Graph() for i in range(hierachy_level)]
 
-    def add_connection(self, level: int, node_1: str, node_2: str):
-        self.g[0].add_edge(self.get_unique_node_name(level, node_1),
-                           self.get_unique_node_name(level, node_2),
-                           euclid_distance=self.get_euclidean_distance(
-            self.g[0].nodes[self.get_unique_node_name(level, node_1)]['pos'],
-            self.g[0].nodes[self.get_unique_node_name(level, node_2)]['pos']))
+    def add_connection(self, level: int, label_1: str, label_2: str):
+        self.g[level].add_edge(self.get_unique_name(level, label_1),
+                               self.get_unique_name(level, label_2),
+                               euclid_distance=self.get_euclidean_distance(
+            self.g[level].nodes[self.get_unique_name(level, label_1)]['pos'],
+            self.g[level].nodes[self.get_unique_name(level, label_2)]['pos']))
 
-    def get_euclidean_distance(self, pos_1, pos_2):
+    def get_euclidean_distance(self, pos_1: Tuple, pos_2: Tuple) -> float:
         return ((pos_1[0] - pos_2[0]) ** 2 + (pos_1[1] - pos_2[1]) ** 2) ** 0.5
 
-    def add_node(self, level: int, pos: Tuple, label: str):
-        self.g[level].add_node(self.get_unique_node_name(level, label), pos=pos, label=label)
+    def add_node(self, level: int, parent: str, pos: Tuple, label: str):
+        self.g[level].add_node(self.get_unique_name(level, parent, label), pos=pos, label=label)
 
-    def get_unique_node_name(self, level: int, label: str):
-        return str(level) + '_' + label
+    def get_unique_name(self, level: int, parent: str, label: str) -> str:
+        if parent:
+            return f'{self.g[level-1]["label"]}_{label}'
+        else:
+            return f'{label}'
 
     def draw_single_level(self, level: int):
+        pos_dict = nx.get_node_attributes(self.g[level], 'pos')
+        for node, pos in pos_dict.items():
+            pos_dict[node] = pos[0:2]
+
         nx.draw(self.g[level],
-                pos=nx.get_node_attributes(self.g[0], 'pos'),
-                labels=nx.get_node_attributes(self.g[0], 'label'),
+                pos=pos_dict,
+                labels=nx.get_node_attributes(self.g[level], 'label'),
                 with_labels=True)
 
         plt.show()
@@ -65,17 +94,18 @@ class SHGraph():
         fig = plt.figure()
         ax = fig.add_subplot(111, projection="3d")
         ax.scatter(*node_xyz.T, s=100, ec="w")
+
         for vizedge in edge_xyz:
             ax.plot(*vizedge.T, color="tab:gray")
 
         ax.grid(False)
         # Suppress tick labels
-        for dim in (ax.xaxis, ax.yaxis, ax.zaxis):
+        for dim in (ax.xaxis, ax.yaxis, ax.zaxis):  # type: ignore
             dim.set_ticks([])
         # Set axes labels
         ax.set_xlabel("x")
         ax.set_ylabel("y")
-        ax.set_zlabel("z")
+        ax.set_zlabel("z")  # type: ignore
 
         fig.tight_layout()
         plt.show()
@@ -84,14 +114,13 @@ class SHGraph():
 def main():
 
     G = SHGraph(3)
-    # G.g.add_node('floor_1', _child_graph=floor_1)
-    # G.g.add_node('floor_2', child_graph=floor_2)
-    G.add_node(0, pos=(1, 1), label='corridor')
-    G.add_node(0, pos=(2, 1), label="room_1")
-    G.add_node(0, pos=(0, 1), label="room_2")
+    G.add_node(level=0, parent=None, pos=(0, 0, 0), label='LTC Campus')
+    G.add_node(level=0, parent=pos=(1, 1, 1), label='corridor')
+    G.add_node(level=0, pos=(2, 1, 1), label="room_1")
+    G.add_node(level=0, pos=(0, 1, 1), label="room_2")
     # floor_1.add_node('elevator')
-    G.add_connection(0, 'corridor', 'room_1')
-    G.add_connection(0, 'corridor', 'room_2')
+    G.add_connection(level=0, label_1='corridor', label_2='room_1')
+    G.add_connection(level=0, label_1='corridor', label_2='room_2')
     # G.g.add_edge('floor_1', 'floor_2', type="elevator")
 
     print(G.g[0].number_of_nodes())
