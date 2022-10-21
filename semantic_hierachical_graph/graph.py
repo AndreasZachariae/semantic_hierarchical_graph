25df9d9f541bb29b45c77d5a3f21fba69f0a20ca
Andreas Zachariae <Andreas.Zachariae@h-ka.de>
2022-10-21 12:31:31 +0200

    Add parent child inheritance

--- semantic_hierachical_graph/graph.py
+++ semantic_hierachical_graph/graph.py
@@ -1,7 +1,7 @@
 import networkx as nx
 import matplotlib.pyplot as plt
 # from mpl_toolkits.mplot3d import Axes3D
-from typing import List, Tuple, TypeVar, Generic
+from typing import List, Tuple, TypeVar, Generic, Type
 import numpy as np
 
 node_attributes = {
@@ -31,47 +31,51 @@ hierarchy_levels = {
 # 2. Hierachical graphs
 # 3. Dictionary like structure G["building F"]["floor 1"]["room 1"]
 
-T = TypeVar('T')
+T = TypeVar('T', bound='SHNode')
 
 
 class SHNode(Generic[T]):
-    def __init__(self, unique_name: str, is_root: bool = False, is_leaf: bool = False):
+    def __init__(self, unique_name: str, parent_node, pos: Tuple[float, float, float], is_root: bool = False, is_leaf: bool = False):
         self.unique_name = unique_name
         self.is_root = is_root
         self.is_leaf = is_leaf
-        self.parent_node: SHNode
+        self.pos: Tuple[float, float, float] = pos
+        self.pos_abs: Tuple[float, float, float] = pos + parent_node.pos_abs if not is_root else pos
+        self.parent_node: SHNode = parent_node
         self.child_graph: nx.Graph = nx.Graph()
 
     def add_child(self, name: str, pos: Tuple[float, float, float], is_leaf: bool = False, **data):
         # Child of type SHNode with unique name on that level
 
-        self.child_graph.add_node(SHNode(unique_name=name, is_leaf=is_leaf), name=name, pos=pos, **data)
+        self.child_graph.add_node(SHNode(unique_name=name, parent_node=self, pos=pos, is_leaf=is_leaf),
+                                  name=name, **data)
 
     def add_connection(self, child_name_1: str, child_name_2: str, **data):
         self.child_graph.add_edge(self.get_child(child_name_1), self.get_child(child_name_2), **data)
 
-    def get_child(self, name: str):
-        for node in self.child_graph.nodes:  # type: ignore
+    def get_child(self, name: str) -> T:
+        for node in self.child_graph.nodes:
             if node.unique_name == name:
                 return node
-        return None
+        raise ValueError("Child with name {} not found".format(name))
 
-    def get_childs(self, key=None):
+    def get_childs(self, key=None) -> List[T]:
         if key is None:
-            return self.child_graph.nodes()
+            return self.child_graph.nodes()  # type: ignore
         else:
-            return [value for n, value in self.child_graph.nodes(data=key)]
+            return [value for n, value in self.child_graph.nodes(data=key)]  # type: ignore
 
-    def get_dict(self):
+    def get_dict(self) -> dict:
         s = {}
-        for n, data in self.child_graph.nodes(data=True):
-            s[data["name"]] = n.get_dict()  # type: ignore
+        for node in self.child_graph.nodes:
+            s[node.unique_name] = node.get_dict()
         return s
 
+    def get_parent_node(self):
+        return self.parent_node
+
     def draw_child_graph(self):
-        pos_dict = nx.get_node_attributes(self.child_graph, 'pos')
-        for node, pos in pos_dict.items():
-            pos_dict[node] = pos[0:2]
+        pos_dict = {node: node.pos_abs[0:2] for node in self.get_childs()}
 
         nx.draw(self.child_graph,
                 pos=pos_dict,
@@ -81,12 +85,18 @@ class SHNode(Generic[T]):
         plt.show()
 
 
-class SHGraph():
+class SHGraph(SHNode):
     def __init__(self, levels: int, root_name: str, root_pos: Tuple[float, float, float]):
+        self.root_graph = nx.Graph()
+        self.root_graph.add_node(self, name=root_name)
+        super().__init__(unique_name=root_name,
+                         parent_node=self.root_graph[self], pos=root_pos, is_root=True)
         self.levels = levels
-        self.root_node: SHNode = SHNode(unique_name=root_name, is_root=True)
 
     def add_node(self, level: int, name: str, parent: str, pos: Tuple[float, float, float], **data):
+        pass
+
+    def create_graph_from_dict(self):
         pass
 
 
@@ -173,21 +183,31 @@ def main():
     # G.draw_multiple_level()
 
     # G = nx.Graph(name="LTC Campus", level=0, pos=(0, 0, 0))
-    # G.add_node(nx.Graph(), name="Building F", level=1, parent=G, pos=(1, 1, 0))
+    # child = nx.Graph()
+    # G.add_node(child, name="Building F", level=1, parent=G, pos=(1, 1, 0))
     # G.add_node(nx.Graph(), name="Building A", level=1, parent=G, pos=(1, 0, 0))
+    # print(G.nodes[child])
     # print([data["name"] for n, data in G.nodes(data=True)])
 
-    SHG = SHGraph(levels=5, root_name="LTC Campus", root_pos=(0, 0, 0))
-    SHG.add_node(level=0, name="Building F", parent="LTC Campus", pos=(1, 1, 0))
+    G = SHGraph(levels=5, root_name="LTC Campus", root_pos=(0, 0, 0))
+    # G.add_node(level=0, name="Building F", parent="LTC Campus", pos=(1, 1, 0))
 
-    G = SHNode(unique_name="LTC Campus", is_root=True)
+    # G = SHNode(unique_name="LTC Campus", is_root=True)
 
     G.add_child(name="Building F", pos=(1, 1, 0))
     G.add_child(name="Building A", pos=(1, 0, 0))
     G.add_connection("Building F", "Building A", name="path_1", type="path")
+    G.get_child("Building F").add_child(name="Floor 1", pos=(1, 1, 1))
+    G.get_child("Building F").add_child(name="Floor 2", pos=(1, 1, 2))
+    G.get_child("Building F").add_child(name="Floor 3", pos=(1, 1, 3))
+    G.get_child("Building A").add_child(name="Floor 1", pos=(1, 0, 1))
+    G.get_child("Building A").add_child(name="Floor 2", pos=(1, 0, 2))
+    G.get_child("Building A").add_child(name="Floor 3", pos=(1, 0, 3))
+    G.get_child("Building F").get_child("Floor 1").add_child(name="Room 1", pos=(1, 1, 1))
     print(G.get_dict())
     print(G.get_childs("name"))
     G.draw_child_graph()
+    G.get_child("Building F").draw_child_graph()
 
 
 if __name__ == "__main__":
