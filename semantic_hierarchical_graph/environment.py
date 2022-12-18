affe69500e6be257d88676f81163d3584289088b
Andreas Zachariae <Andreas.Zachariae@h-ka.de>
2022-12-18 19:32:01 +0100

    Merge collision checker to environment

--- semantic_hierarchical_graph/environment.py
+++ semantic_hierarchical_graph/environment.py
@@ -1,26 +1,57 @@
-from typing import List
+from typing import Any, List
 import numpy as np
-from semantic_hierarchical_graph.collision_checker import CollisionChecker
-from descartes.patch import PolygonPatch
 import matplotlib.pyplot as plt
-from shapely.plotting import plot_polygon, plot_points
+from shapely.plotting import plot_polygon, plot_line
+from shapely.geometry import Point, Polygon, LineString
+from shapely.ops import nearest_points
 
 
-class Environment(CollisionChecker):
-    def __init__(self, name: str, limits: np.ndarray, scene: List = []):
+class Environment():
+    def __init__(self, name: Any, limits: np.ndarray):
         self.name = name
         self.limits = limits
-        super().__init__(scene)
+        self.scene: List[Polygon] = []
+        self.path: List[LineString] = []
 
     def add_obstacle(self, obstacle):
         self.scene.append(obstacle)
 
+    def add_path(self, path):
+        self.path.append(path)
+
+    def point_in_collision(self, pos):
+        """ Return whether a point is
+        in collision -> True
+        Free -> False """
+        for value in self.scene:
+            if value.intersects(Point(pos[0], pos[1])):
+                return True
+        return False
+
+    def line_in_collision(self, start_pos, end_pos):
+        """ Check whether a line from start_pos to end_pos is colliding"""
+        for value in self.scene:
+            if value.intersects(LineString([(start_pos[0], start_pos[1]), (end_pos[0], end_pos[1])])):
+                return True
+        return False
+
+    def add_shortest_connection(self, pos):
+        """ Find the shortest path from pos to any path that is not in collision """
+        closest_path = min(self.path, key=lambda x: x.distance(Point(pos[0], pos[1])))
+        closest_point: Point = nearest_points(closest_path, Point(pos[0], pos[1]))[0]
+        # if self.line_in_collision(closest_point.coords._coords[0], (pos[0], pos[1])):
+        #     print("Connection in collision")
+        connection = LineString([closest_point, Point(pos[0], pos[1])])
+        self.add_path(connection)
+
     def plot(self):
         fig, ax = plt.subplots(figsize=(10, 10))
-        print(len(self.scene))
+        ax.invert_yaxis()
+        ax.set_aspect("equal")
         for value in self.scene:
-            print(value)
-            # patch = PolygonPatch(value, facecolor="red", alpha=0.8, zorder=2)
-            # ax.add_patch(patch)
-            plot_polygon(value, ax=ax, facecolor="red", alpha=0.8, zorder=2)
+            plot_polygon(value, ax=ax, add_points=False, color="red", alpha=0.8)
+
+        for value in self.path:
+            plot_line(value, ax=ax, add_points=False, color="blue", alpha=0.8)
+
         plt.show()
