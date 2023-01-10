fceec8c0bc775dc7c9f5b36d32ff54e4d2eec30a
Andreas Zachariae <Andreas.Zachariae@h-ka.de>
2023-01-10 18:51:17 +0100

    Add parameter file for different maps

---
+++ semantic_hierarchical_graph/parameters.py
@@ -0,0 +1,36 @@
+from typing import Tuple, Any
+import yaml
+from semantic_hierarchical_graph.utils import round_up
+
+
+class Parameter():
+    def __init__(self, path: str):
+        self.params: dict[str, Any] = self.load_params(path)
+        self.params["safety_distance"] = self.get_safety_distance(
+            self.params["base_size"], self.params["safety_margin"])
+
+    def load_params(self, path: str):
+        with open(path, "r") as file:
+            params = {}
+            try:
+                params = yaml.safe_load(file)
+            except yaml.YAMLError as e:
+                print(e)
+        return params
+
+    def get_safety_distance(self, base_size: Tuple[int, int], safety_margin: int) -> int:
+        """
+        Parameters
+        ----------
+        base_size : Tuple[int, int]
+            (width from side to side, length in drive direction)
+        safety_margin : int
+            distance around robot as: (safety_margin + base_size[0] + safety_margin)
+
+        Returns
+        -------
+        int
+            distance from imaginary point robot
+        """
+        # TODO: extend to circle and with trailer
+        return round_up(base_size[0]/2 + safety_margin)
