39f61a6e3afbe0677216fa0612e6d273f2e6045a
AndreasZachariae <61884882+AndreasZachariae@users.noreply.github.com>
2023-02-07 12:39:24 +0100

    Add metrics

--- semantic_hierarchical_graph/metrics.py
+++ semantic_hierarchical_graph/metrics.py
@@ -10,15 +10,15 @@ class Metrics():
         self.metrics["id"] = room.id
         self.metrics.update(room.params)
         self.metrics["num_nodes"] = len(room.get_childs())
-        self.metrics["average_line_length"] = self._calc_average_line_length(room.env)
+        self.metrics["path_length"] = sum([path.length for path in room.env.path])
+        self.metrics["average_line_length"] = self.metrics["path_length"] / len(room.env.path)
         self.metrics["num_turns"] = 0  # define comparable start end end points
         self.metrics["cumulative_turning_angle"] = 0
         self.metrics["smoothness"] = self._calc_smoothness(room.env)
         self.metrics["planning_time"] = 0
         self.metrics["public_disturbance"] = self._calc_public_disturbance(room.env)
-
-    def _calc_average_line_length(self, env) -> float:
-        return sum([path.length for path in env.path]) / len(env.path)
+        self.metrics["success rate"] = 0 # over multiple runs. For PRB between same goals or for Deterministic between all points of a floor
+        self.metrics["obstacle_clearance"] = self._calc_obstacle_clearance(room.env)
 
     def _calc_smoothness(self, env) -> float:
         smoothness = 0
@@ -30,7 +30,16 @@ class Metrics():
         # Normalize to path length
         # Rectangles with 90 degree turns always have the same smoothness
         # Sum up all turning angles and divide by the number of turns or divied by the path length
+        
+        # Alternative paper https://arxiv.org/pdf/2203.03092.pdf
+        # Smoothness of trajectory (degrees). The average angle change between consecutive segments of paths shows how drastic and sudden the agent’s movement changes could be
 
+        # Alternative paper
+        # The path smoothness can be calculated by using the following formula:
+#         S(P) = XDi=1αi =XDi=1 arccos((Pi − Pi−1) · (Pi+1 − Pi)|Pi − Pi−1| × |Pi+1 − Pi| × 180)
+#         where αi refers to the value of the i-th deflection angle of the generated path (measured in radians in the
+#         range from 0 to π). (Pi − Pi−1) · (Pi+1 − Pi) indicates the inner product between vectors of Pi − Pi−1
+#         and Pi+1 − Pi while |Pi − Pi−1| denotes the vector norm.
         return smoothness
 
     def _calc_public_disturbance(self, env) -> float:
