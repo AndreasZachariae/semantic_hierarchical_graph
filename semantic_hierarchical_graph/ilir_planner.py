5abb75d3c4f65e2ae45d1e3e2c9a585ec992b859
Andreas Zachariae <andreas.zachariae@hs-karlsruhe.de>
2023-02-19 18:51:05 +0100

    Add method to add new nodes to the graph and connect start or goal points which are not on the roadmap.

--- semantic_hierarchical_graph/ilir_planner.py
+++ semantic_hierarchical_graph/ilir_planner.py
@@ -1,7 +1,10 @@
 from typing import List, Tuple
+
+from shapely import Point
 from semantic_hierarchical_graph.graph import SHGraph
 from semantic_hierarchical_graph.floor import Room, Floor
-from semantic_hierarchical_graph import visualization as vis
+from semantic_hierarchical_graph import path_planning, segmentation, visualization as vis
+from semantic_hierarchical_graph.node import SHNode
 import semantic_hierarchical_graph.utils as util
 
 
@@ -10,28 +13,60 @@ Position = Tuple[float, float, float]
 class ILIRPlanner():
     def __init__(self, room: Room):
         self.room = room
+        self.original_graph = room.child_graph.copy()
+        self.original_leaf_graph = room._get_root_node().leaf_graph.copy()
+        self.tmp_graph = room.child_graph
 
     def plan(self, start: Position, goal: Position):
-        grid_start = self._convert_position_to_grid(start)
-        grid_goal = self._convert_position_to_grid(goal)
-        start_path, goal_path = [], []
-        if str(grid_start) not in self.room.get_childs("name"):
-            start_path = self._path_to_roadmap(grid_start)
-        if str(grid_goal) not in self.room.get_childs("name"):
-            goal_path = self._path_to_roadmap(grid_goal)
+        start_name, start_pos = self._convert_position_to_grid_node(start)
+        goal_name, goal_pos = self._convert_position_to_grid_node(goal)
+        if start_name not in self.room.get_childs("name"):
+            self._add_path_to_roadmap(start_name, start_pos, type = "start")
+        if goal_name not in self.room.get_childs("name"):
+            self._add_path_to_roadmap(goal_name, goal_pos, type = "goal")
 
-        path = self.room._plan(str(grid_start), str(grid_goal))
-        path.extend(goal_path)
-        start_path.extend(path)
+        path = self.room._plan(start_name, goal_name)
 
-        return start_path
+        # Reset graph
+        self.room.child_graph = self.original_graph
+        self.room._get_root_node().leaf_graph = self.original_leaf_graph
 
-    def _convert_position_to_grid(self, pos: Position) -> Tuple[int, int]:
+        return path
+
+    def _convert_position_to_grid_node(self, pos: Position) -> Tuple[str, Position]:
         # TODO: Convert meters from map frame to pixels in grid map
-        return (round(pos[0]), round(pos[1]))
+        return str((round(pos[0]), round(pos[1]))), pos
 
-    def _path_to_roadmap(self, node) -> List:
-        return []
+    def _add_path_to_roadmap(self, node_name, node_pos, type):
+        if self.room.env._in_collision(Point(node_pos[:2])):
+            raise Exception(f"Point {node_pos} is not in the drivable area (boundary + safety margin) of the room")
+        
+        connections, closest_path = path_planning.connect_point_to_path(node_pos[:2], self.room.env, self.room.params)
+
+        if len(connections) == 0:
+            raise Exception(f"No connection from point {node_pos} to roadmap found")
+        
+        prev_node = self.room.add_child_by_name(node_name, node_pos, True, type=type)
+        for connection in connections:
+            for point in connection.coords:
+                p = str((round(point[0]), round(point[1])))
+                cur_node = self.room.add_child_by_name(p, point + (0.0,), True, type="aux_node")
+                self.room.add_connection_by_nodes(prev_node, cur_node, 
+                                                  util.get_euclidean_distance(prev_node.pos, cur_node.pos))
+                prev_node = cur_node
+
+        if len(closest_path.coords) != 2:
+            raise ValueError("Path has not 2 points as expected")
+        path_1_node = self.room._get_child(str((round(closest_path.coords[0][0]), round(closest_path.coords[0][1]))))
+        path_2_node = self.room._get_child(str((round(closest_path.coords[1][0]), round(closest_path.coords[1][1]))))
+        self.room.child_graph.remove_edge(path_1_node, path_2_node)
+        self.room.add_connection_by_nodes(path_1_node, prev_node, 
+                                          util.get_euclidean_distance(closest_path.coords[0]+ (0.0,), prev_node.pos))
+        self.room.add_connection_by_nodes(prev_node, path_2_node, 
+                                          util.get_euclidean_distance(closest_path.coords[1]+ (0.0,), prev_node.pos))
+
+        # vis.draw_child_graph_3d(self.room)
+
 
 if __name__ == "__main__":
     G = SHGraph(root_name="Benchmark", root_pos=(0, 0, 0))
@@ -46,8 +81,10 @@ if __name__ == "__main__":
     room_11 = floor._get_child("room_11")
     room_14 = floor._get_child("room_14")
     planner = ILIRPlanner(room_11)
-    path = planner.plan((555, 211), (81, 358))
+    # TODO: test for goal point not on graph
+    path = planner.plan((480, 250, 0), (81, 358, 0))
+    # path = planner.plan((555, 211, 0), (81, 358, 0))
     path_list = util._map_names_to_nodes(path)
 
     print(path_list)
-    vis.draw_child_graph_3d(room_11, path)
+    vis.draw_child_graph_3d(room_11, path)  # type: ignore
