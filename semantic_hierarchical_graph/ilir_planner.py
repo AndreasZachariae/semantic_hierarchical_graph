58b263e87bafaaaa925f58502f1644ed99ea3b89
Andreas Zachariae <Andreas.Zachariae@h-ka.de>
2023-02-20 19:00:04 +0100

    Fic correct reset of modified graphs. WARNING this is very slow

--- semantic_hierarchical_graph/ilir_planner.py
+++ semantic_hierarchical_graph/ilir_planner.py
@@ -1,7 +1,8 @@
 from collections import deque
 from typing import List, Tuple, Union
+from copy import deepcopy, copy
 
-from shapely import Point
+from shapely import LineString, Point
 from semantic_hierarchical_graph.exceptions import SHGGeometryError, SHGPlannerError
 from semantic_hierarchical_graph.graph import SHGraph
 from semantic_hierarchical_graph.floor import Room, Floor
@@ -13,11 +14,21 @@ from semantic_hierarchical_graph.types import Position
 class ILIRPlanner():
     def __init__(self, room: Room):
         self.room = room
-        self.original_graph = room.child_graph.copy()
-        self.original_leaf_graph = room._get_root_node().leaf_graph.copy()
-        self.tmp_graph = room.child_graph
+
+    def _copy_graph(self):
+        # TODO: This is a hack to make sure that the graph is not modified by the planner
+        #       This takes very long and slows down the planning
+        self.original_path = deepcopy(self.room.env.path)
+        self.original_graph = deepcopy(self.room.child_graph)
+        self.original_leaf_graph = deepcopy(self.room._get_root_node().leaf_graph)
+
+    def _reset_graph(self):
+        self.room.env.path = self.original_path
+        self.room.child_graph = self.original_graph
+        self.room._get_root_node().leaf_graph = self.original_leaf_graph
 
     def plan(self, start: Union[Tuple, List, Position], goal: Union[Tuple, List, Position]):
+        self._copy_graph()
         try:
             start_name, start_pos = self._convert_position_to_grid_node(start)
             goal_name, goal_pos = self._convert_position_to_grid_node(goal)
@@ -32,11 +43,10 @@ class ILIRPlanner():
             print(e)
             path = None
         finally:
-            # Reset graph
-            self.room.child_graph = self.original_graph
-            self.room._get_root_node().leaf_graph = self.original_leaf_graph
+            vis_graph = self.room.child_graph
+            self._reset_graph()
 
-        return path
+        return path, vis_graph
 
     def _convert_position_to_grid_node(self, pos: Union[Tuple, List, Position]) -> Tuple[str, Position]:
         # TODO: Convert meters from map frame to pixels in grid map
@@ -52,7 +62,7 @@ class ILIRPlanner():
         connections, closest_path = path_planning.connect_point_to_path(node_pos.xy, self.room.env, self.room.params)
 
         if len(connections) == 0:
-            raise SHGPlannerError(f"No connection from point {node_pos} to roadmap found")
+            raise SHGPlannerError(f"No connection from point {node_pos.xy} to roadmap found")
 
         nodes = deque(maxlen=2)
         nodes.append(self.room.add_child_by_name(node_name,
@@ -71,12 +81,12 @@ class ILIRPlanner():
         path_2_pos = Position.from_iter(closest_path.coords[1])
         path_1_node = self.room._get_child(path_1_pos.to_name())
         path_2_node = self.room._get_child(path_2_pos.to_name())
-        # print(self.room.child_graph.edges)
         self.room.child_graph.remove_edge(path_1_node, path_2_node)
+        self.room.env.path.remove(closest_path)
         self.room.add_connection_by_nodes(path_1_node, nodes[1], path_1_pos.distance(nodes[1].pos))
         self.room.add_connection_by_nodes(nodes[1], path_2_node, path_2_pos.distance(nodes[1].pos))
-
-        # vis.draw_child_graph_3d(self.room)
+        self.room.env.add_path(LineString([path_1_pos.xy, nodes[1].pos.xy]))
+        self.room.env.add_path(LineString([nodes[1].pos.xy, path_2_pos.xy]))
 
 
 if __name__ == "__main__":
@@ -93,9 +103,9 @@ if __name__ == "__main__":
     room_14 = floor._get_child("room_14")
     planner = ILIRPlanner(room_11)
     # segmentation.show_imgs(room_11.mask)
-    path = planner.plan((480, 250), (75, 260))
+    path, vis_graph = planner.plan((480, 250), (75, 260))
     # path = planner.plan((555, 211), (81, 358))
     path_list = util._map_names_to_nodes(path)
 
     print(path_list)
-    vis.draw_child_graph_3d(room_11, path)  # type: ignore
+    vis.draw_child_graph_3d(room_11, path, vis_graph)
