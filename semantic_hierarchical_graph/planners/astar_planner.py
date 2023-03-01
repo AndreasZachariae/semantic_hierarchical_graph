7cd86653f051b5fbd24d20d7ac349b91bb22bb0a
Andreas Zachariae <andreas.zachariae@gmail.com>
2023-03-01 11:21:51 +0100

    Add testing function

--- semantic_hierarchical_graph/planners/astar_planner.py
+++ semantic_hierarchical_graph/planners/astar_planner.py
@@ -32,3 +32,30 @@ class AStarPlanner():
     def plan_in_map_frame(self, start: Tuple, goal: Tuple):
         start_pos, goal_pos = pc.convert_map_frame_to_grid(start, goal, self.room.params["grid_size"])
         self.plan(start_pos, goal_pos)
+
+
+if __name__ == "__main__":
+    from semantic_hierarchical_graph.graph import SHGraph
+    from semantic_hierarchical_graph.floor import Room, Floor
+    import semantic_hierarchical_graph.visualization as vis
+    import semantic_hierarchical_graph.utils as util
+    from semantic_hierarchical_graph.types.position import Position
+    G = SHGraph(root_name="Benchmark", root_pos=Position(0, 0, 0))
+    floor = Floor("ryu", G, Position(0, 0, 1), 'data/benchmark_maps/ryu.png', "config/ryu_params.yaml")
+    G.add_child_by_node(floor)
+    print(G.get_childs("name"))
+
+    floor.create_rooms()
+    floor.create_bridges()
+
+    room_2 = floor._get_child("room_2")
+    room_11 = floor._get_child("room_11")
+    room_14 = floor._get_child("room_14")
+    planner = AStarPlanner(room_11)
+    # segmentation.show_imgs(room_11.mask)
+    path, vis_graph = planner.plan((480, 250), (75, 260))
+    # path = planner.plan((555, 211), (81, 358))
+    path_list = util._map_names_to_nodes(path)
+
+    print(path_list)
+    # vis.draw_child_graph_3d(room_11, path, vis_graph)
