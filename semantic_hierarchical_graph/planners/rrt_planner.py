171d59d600333345993220accc794f230a89c6cd
Andreas Zachariae <andreas.zachariae@gmail.com>
2023-03-02 18:16:47 +0100

    Add new constructor to plan with rrt to connect a point to path

--- semantic_hierarchical_graph/planners/rrt_planner.py
+++ semantic_hierarchical_graph/planners/rrt_planner.py
@@ -1,4 +1,5 @@
-from typing import Tuple
+from typing import Any, List, Optional, Tuple
+from path_planner_suite.IPEnvironment import CollisionChecker
 
 from path_planner_suite.IPRRT import RRT
 from semantic_hierarchical_graph.floor import Room
@@ -6,15 +7,30 @@ import semantic_hierarchical_graph.planners.planner_conversion as pc
 
 
 class RRTPlanner():
-    def __init__(self, room: Room):
+    def __init__(self, room: Room, config: Optional[dict] = None):
         self.name = "RRT"
         self.room = room
-        self.config = dict()
-        self.config["numberOfGeneratedNodes"] = 300
-        self.config["testGoalAfterNumberOfNodes"] = 10
+        if config is None:
+            self.config = dict()
+            self.config["numberOfGeneratedNodes"] = 300
+            self.config["testGoalAfterNumberOfNodes"] = 10
+        else:
+            self.config = config
 
         self.benchmark = pc.convert_room_to_IPBenchmark(room)
         self.planner = RRT(self.benchmark.collisionChecker)
+
+    @classmethod
+    def around_point(cls, point: Tuple[float, float], max_dist: float, scene: List, config: Optional[dict] = None):
+        obj = cls.__new__(cls)
+        obj.name = "RRT"
+        obj.config = config
+        IPlimits = [[round(point[0] - max_dist), round(point[0] + max_dist)],
+                    [round(point[1] - max_dist), round(point[1] + max_dist)]]
+        IPscene = {str(i): obstacle for i, obstacle in enumerate(scene)}
+        IPcollision_checker = CollisionChecker(IPscene, IPlimits)
+        obj.planner = RRT(IPcollision_checker)
+        return obj
 
     def plan(self, start: Tuple, goal: Tuple):
         start_list, goal_list = pc.convert_to_start_goal_lists(start, goal)
