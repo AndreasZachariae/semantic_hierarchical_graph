384ee479dcd71d9243bce60a839edccf02047d76
Andreas Zachariae <andreas.zachariae@gmail.com>
2023-03-04 22:56:23 +0100

    Fix map paths

--- semantic_hierarchical_graph/segmentation.py
+++ semantic_hierarchical_graph/segmentation.py
@@ -145,10 +145,10 @@ def show_imgs(img: np.ndarray, img_2: np.ndarray = None, name: str = None, save=
 
 
 if __name__ == '__main__':
-    img = cv2.imread('data/hou2_clean.png')
-    # img = cv2.imread('data/ryu.png')
-    # params = Parameter("config/ryu_params.yaml").params
-    params = Parameter("config/hou2_params.yaml").params
+    # img = cv2.imread('data/benchmark_maps/hou2_clean.png')
+    img = cv2.imread('data/benchmark_maps/ryu.png')
+    params = Parameter("config/ryu_params.yaml").params
+    # params = Parameter("config/hou2_params.yaml").params
 
     ws, ws_erosion, dist_transform = marker_controlled_watershed(img, params)
     bridge_nodes, bridge_edges = find_bridge_nodes(ws_erosion, dist_transform)
