348aa11e82ace28ea16bfaf935cfe156279f7449
Andreas Zachariae <andreas.zachariae@gmail.com>
2023-05-19 21:06:54 +0200

    Only building in script

--- build_docker.sh
+++ build_docker.sh
@@ -4,21 +4,4 @@ gid=$(eval "id -g")
 docker build \
     --build-arg UID="$uid" \
     --build-arg GID="$gid" \
-    -t shg/ros:foxy . && \
-
-echo "Run Container" && \
-xhost + local:root && \
-docker run \
-    --name semantic_hierarchical_graph \
-    -it \
-    --privileged \
-    --net host \
-    --env-file .env \
-    -e DISPLAY=$DISPLAY \
-    --rm \
-    -v ./semantic_hierarchical_graph:/home/docker/ros2_ws/src/semantic_hierarchical_graph/semantic_hierarchical_graph:rw \
-    -v ./path_planner_suite:/home/docker/ros2_ws/src/semantic_hierarchical_graph/path_planner_suite:rw \
-    -v ./ros2:/home/docker/ros2_ws/src/semantic_hierarchical_graph/ros2:rw \
-    -v ./data/graphs:/home/docker/ros2_ws/src/semantic_hierarchical_graph/data/graphs:rw \
-    -v ./.vscode:/home/docker/.vscode:rw \
-    shg/ros:foxy
+    -t shg/ros:foxy .
