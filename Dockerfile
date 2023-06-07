22aed6f68a2b7988a4779981538e2873b44d515e
Andreas Zachariae <andreas.zachariae@gmail.com>
2023-06-07 22:06:57 +0200

    Add neobotix model in simulation

--- Dockerfile
+++ Dockerfile
@@ -16,13 +16,18 @@ RUN rosdep update --rosdistro $ROS_DISTRO
 RUN apt-get update && apt-get install --no-install-recommends -y \
     python3-pip \
     ffmpeg libsm6 libxext6  \
+    python3-colcon-common-extensions \
     ros-$ROS_DISTRO-navigation2 \
-    ros-$ROS_DISTRO-nav2-bringup \
+    ros-$ROS_DISTRO-nav2-* \
     ros-$ROS_DISTRO-turtlebot3-* \
     ros-$ROS_DISTRO-rviz2 \
     ros-$ROS_DISTRO-gazebo-* \
     ros-$ROS_DISTRO-tf-transformations \
     ros-$ROS_DISTRO-rqt* \
+    ros-$ROS_DISTRO-slam-toolbox \
+    ros-$ROS_DISTRO-teleop-twist-joy \
+    ros-$ROS_DISTRO-teleop-twist-keyboard \
+    xterm \
     qtbase5-dev \
     libqt5svg5-dev \
     libzmq3-dev \
@@ -67,7 +72,13 @@ COPY ./aws-robomaker-hospital-world ./aws-robomaker-hospital-world
 # RUN python3 aws-robomaker-hospital-world/fuel_utility.py download -m XRayMachine -m IVStand -m BloodPressureMonitor -m BPCart -m BMWCart -m CGMClassic -m StorageRack -m Chair -m InstrumentCart1 -m Scrubs -m PatientWheelChair -m WhiteChipChair -m TrolleyBed -m SurgicalTrolley -m PotatoChipChair -m VisitorKidSit -m FemaleVisitorSit -m AdjTable -m MopCart3 -m MaleVisitorSit -m Drawer -m OfficeChairBlack -m ElderLadyPatient -m ElderMalePatient -m InstrumentCart2 -m MetalCabinet -m BedTable -m BedsideTable -m AnesthesiaMachine -m TrolleyBedPatient -m Shower -m SurgicalTrolleyMed -m StorageRackCovered -m KitchenSink -m Toilet -m VendingMachine -m ParkingTrolleyMin -m PatientFSit -m MaleVisitorOnPhone -m FemaleVisitor -m MalePatientBed -m StorageRackCoverOpen -m ParkingTrolleyMax \
 #     -d aws-robomaker-hospital-world/fuel_models --verbose
 
-RUN git clone --branch $ROS_DISTRO https://github.com/neobotix/neo_local_planner2.git
+RUN git clone --branch $ROS_DISTRO     https://github.com/neobotix/neo_simulation2.git
+RUN git clone --branch galactic     https://github.com/neobotix/neo_nav2_bringup.git
+RUN git clone --branch $ROS_DISTRO     https://github.com/neobotix/neo_local_planner2.git
+RUN git clone --branch galactic     https://github.com/neobotix/neo_localization2.git
+RUN git clone --branch master          https://github.com/neobotix/neo_common2
+RUN git clone --branch master          https://github.com/neobotix/neo_msgs2
+RUN git clone --branch master          https://github.com/neobotix/neo_srvs2
 
 # For semantic_hierarchical_graph
 WORKDIR /home/$USER/ros2_ws/src/semantic_hierarchical_graph
@@ -95,6 +106,10 @@ RUN echo "source /home/$USER/ros2_ws/install/setup.bash" >> /home/$USER/.bashrc
 
 RUN sudo sed --in-place --expression \
     '$isource "/home/$USER/ros2_ws/install/setup.bash"' \
+    /ros_entrypoint.sh
+
+RUN sudo sed --in-place --expression \
+    '$isource "/usr/share/gazebo/setup.sh"' \
     /ros_entrypoint.sh
 
 CMD /bin/bash
---
+++ ros2/launch/neobotix_simulation.launch.py
@@ -0,0 +1,126 @@
+# Neobotix GmbH
+# Author: Pradheep Padmanabhan
+
+import launch
+from ament_index_python.packages import get_package_share_directory
+from launch import LaunchDescription
+from launch.actions import DeclareLaunchArgument, IncludeLaunchDescription, ExecuteProcess
+from launch.launch_description_sources import PythonLaunchDescriptionSource
+from launch.substitutions import ThisLaunchFileDir, LaunchConfiguration
+from launch_ros.actions import Node
+import os
+from pathlib import Path
+
+MY_NEO_ROBOT = os.environ['MY_ROBOT']
+MY_NEO_ENVIRONMENT = os.environ['MAP_NAME']
+
+
+def generate_launch_description():
+    aws_hospital_dir = get_package_share_directory('aws_robomaker_hospital_world')
+    default_world_path = os.path.join(aws_hospital_dir, 'worlds', MY_NEO_ENVIRONMENT + '.world')
+
+    # default_world_path = os.path.join(get_package_share_directory(
+    #     'neo_simulation2'), 'worlds', MY_NEO_ENVIRONMENT + '.world')
+
+    use_sim_time = LaunchConfiguration('use_sim_time', default='true')
+
+    robot_dir = LaunchConfiguration(
+        'robot_dir',
+        default=os.path.join(get_package_share_directory('neo_simulation2'),
+                             'robots/'+MY_NEO_ROBOT,
+                             MY_NEO_ROBOT+'.urdf'))
+
+    # declare_use_sim_time_cmd = DeclareLaunchArgument(
+    #     'use_sim_time',
+    #     default_value='false',
+    #     description='Use simulation (Gazebo) clock if true')
+
+    use_robot_state_pub = LaunchConfiguration('use_robot_state_pub')
+
+    urdf = os.path.join(get_package_share_directory('neo_simulation2'),
+                        'robots/'+MY_NEO_ROBOT+'/', MY_NEO_ROBOT+'.urdf')
+
+    spawn_entity = Node(package='gazebo_ros', executable='spawn_entity.py', arguments=[
+                        '-entity', MY_NEO_ROBOT, '-file', urdf, '-x', '2', '-y', '2'], output='screen')
+
+    rviz_config_dir = os.path.join(
+        get_package_share_directory('nav2_bringup'),
+        'rviz',
+        'nav2_default_view.rviz')
+
+    start_robot_state_publisher_cmd = Node(
+        package='robot_state_publisher',
+        executable='robot_state_publisher',
+        name='robot_state_publisher',
+        output='screen',
+        parameters=[{'use_sim_time': use_sim_time}],
+        arguments=[urdf])
+
+    teleop = Node(package='teleop_twist_keyboard', executable="teleop_twist_keyboard",
+                  output='screen',
+                  prefix='xterm -e',
+                  name='teleop')
+
+    gazebo = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource(
+            os.path.join(get_package_share_directory('gazebo_ros'), 'launch', 'gazebo.launch.py')
+        ),
+        launch_arguments={
+            'world': default_world_path,
+            'verbose': 'true'
+        }.items()
+    )
+
+    rviz = Node(
+        package='rviz2',
+        executable='rviz2',
+        name='rviz2',
+        arguments=['-d', rviz_config_dir],
+        parameters=[{'use_sim_time': use_sim_time}],
+        output='screen')
+
+    # NAVIGATION
+
+    use_sim_time = LaunchConfiguration('use_sim_time', default='true')
+    map_dir = LaunchConfiguration(
+        'map',
+        default=os.path.join(aws_hospital_dir, 'maps', 'aws_robomaker_hospital.yaml'))
+
+    param_file_name = 'navigation.yaml'
+    param_dir = LaunchConfiguration(
+        'params_file',
+        default=os.path.join(
+            get_package_share_directory('neo_simulation2'),
+            'configs/'+MY_NEO_ROBOT,
+            param_file_name))
+
+    nav2_launch_file_dir = os.path.join(get_package_share_directory('nav2_bringup'), 'launch')
+
+    return LaunchDescription([
+        DeclareLaunchArgument(
+            'map',
+            default_value=map_dir,
+            description='Full path to map file to load'),
+
+        DeclareLaunchArgument(
+            'params_file',
+            default_value=param_dir,
+            description='Full path to param file to load'),
+
+        DeclareLaunchArgument(
+            'use_sim_time',
+            default_value='true',
+            description='Use simulation (Gazebo) clock if true'),
+
+        IncludeLaunchDescription(
+            PythonLaunchDescriptionSource([nav2_launch_file_dir, '/bringup_launch.py']),
+            launch_arguments={
+                'map': map_dir,
+                'use_sim_time': use_sim_time,
+                'params_file': param_dir}.items(),
+        ),
+        spawn_entity,
+        start_robot_state_publisher_cmd,
+        teleop,
+        gazebo,
+        rviz])
